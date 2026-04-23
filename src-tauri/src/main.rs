77a78b511898dcd2fd54a166c096a4f777f94793
lutgaru <lutgaru@gmail.com>
2026-04-22 18:52:59 -0700

    add licence header to files

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,14 @@
+/*
+ * Babilo - Copyright (C) 2026 Lutgaru
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+ 
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
--- src-tauri/src/utils/mod.rs
+++ src-tauri/src/utils/mod.rs
@@ -1,3 +1,14 @@
+/*
+ * Babilo - Copyright (C) 2026 Lutgaru
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ */
+
+ 
 //! Utilidades generales
 
 use std::path::{Path, PathBuf};
